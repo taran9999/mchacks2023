740aa567fb260c96c9b31494c36e54f11bb02894
Jake <46657160+jacobkogut@users.noreply.github.com>
2023-01-28 17:37:23 -0500

    command line inputs

--- client4.py
+++ client4.py
@@ -4,11 +4,15 @@ import select
 import sys
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-if len(sys.argv) != 3:
-	print ("Correct usage: script, IP address, port number")
-	exit()
-IP_address = str(sys.argv[1])
-Port = int(sys.argv[2])
+
+#if len(sys.argv) != 3:
+#	print ("Correct usage: script, IP address, port number")
+#	exit()
+
+
+
+IP_address = input("Enter IP Address: ")
+Port = int(input("Enter port: "))
 server.connect((IP_address, Port))
 
 while True:
